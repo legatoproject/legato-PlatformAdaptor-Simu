7bc72eb8960980e77a811659c0fddf0f4526dec5
Eric Charon <ECH@sierrawireless.com>
2017-07-18 15:45:49 +0200

    Correct le_info_GetRfDeviceStatus API
    Add LE_BAD_PARAMETER error code.
    
    Resolves: LE-6700
    Change-Id: I53b814b04394d6c6734db8b888bf517dbc8b85dd

--- components/le_pa/pa_info_simu.c
+++ components/le_pa/pa_info_simu.c
@@ -897,6 +897,7 @@ le_result_t pa_info_GetPlatformSerialNumber
  *      - LE_FAULT function failed to get the RF devices working status
  *      - LE_OVERFLOW the number of statuses exceeds the maximum size
  *        (LE_INFO_RF_DEVICES_STATUS_MAX)
+ *      - LE_BAD_PARAMETER Null pointers provided
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t pa_info_GetRfDeviceStatus
